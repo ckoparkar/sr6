facb5a8bc9095775f826dd55a8d262fd9ab5a99c
cskksc <ckoparkar@live.in>
2015-10-27 14:31:38 +0530

    [gru] Start with sending back ssh and host information.

--- main.go
+++ main.go
@@ -9,13 +9,21 @@ import (
 )
 
 var (
-	listenAddr   = flag.String("listen", ":8281", "HTTP listen adddress.")
-	pollInterval = flag.Duration("poll", time.Minute, "Polls followers every `t`.")
+	listenAddr        = flag.String("listen", ":8281", "HTTP listen adddress.")
+	pollInterval      = flag.Duration("poll", time.Minute, "Polls followers every `t`.")
+	sshPrivateKeyPath = flag.String("ssh-private-key", "~/.ssh/id_rsa", "Path to SSH private key.")
+	sshPublicKeyPath  = flag.String("ssh-public-key", "~/.ssh/id_rsa.pub", "Path to SSH public key.")
+	// must contain ID
+	hostnamePattern = flag.String("hostname", "minionID", "ID in `S` is replaced with actual ids.")
+	followerPort    = flag.String("follower-port", ":8282", "Port on which all follower servers run.")
 )
 
 func main() {
 	flag.Parse()
-	s := NewServer()
+	s, err := NewServer()
+	if err != nil {
+		log.Fatal(err)
+	}
 	http.Handle("/", s)
 	fmt.Printf("Listening for requests on %s...\n", *listenAddr)
 	log.Fatal(http.ListenAndServe(*listenAddr, nil))
--- server.go
+++ server.go
@@ -6,27 +6,51 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/cskksc/minion/request"
 )
 
-const (
-	followerPort = ":8282"
-)
+type SSHKeys struct {
+	private, public []byte
+}
+
+func NewSSHKeys() (*SSHKeys, error) {
+	privateKey, err := ioutil.ReadFile(*sshPrivateKeyPath)
+	if err != nil {
+		return nil, err
+	}
+	publicKey, err := ioutil.ReadFile(*sshPublicKeyPath)
+	if err != nil {
+		return nil, err
+	}
+	return &SSHKeys{
+		private: privateKey,
+		public:  publicKey,
+	}, nil
+}
 
 type Server struct {
+	sshKeys   SSHKeys
 	mu        sync.RWMutex
 	followers []Heartbeat
 }
 
-func NewServer() *Server {
+func NewServer() (*Server, error) {
+	keys, err := NewSSHKeys()
+	if err != nil {
+		return nil, err
+	}
 	s := &Server{
+		sshKeys:   *keys,
 		followers: make([]Heartbeat, 0),
 	}
 	go s.run()
-	return s
+	return s, nil
 }
 
 func (s *Server) run() {
@@ -49,14 +73,16 @@ func (s *Server) poll() {
 	ping := make(chan int)
 	for i := len(s.followers) - 1; i >= 0; i-- {
 		f := s.followers[i]
-		hostport := f.Address + followerPort
+		hostport := f.Address + *followerPort
+		timeout := time.After(10 * time.Millisecond)
 		go func() {
 			req := request.NewRequest("GET", "http", hostport, "/heartbeat", nil, nil)
 			_, _, err := req.Do()
 			if err != nil {
-				log.Println(err)
+				<-timeout
+			} else {
+				ping <- 1
 			}
-			ping <- 1
 		}()
 
 		select {
@@ -64,35 +90,47 @@ func (s *Server) poll() {
 		case <-time.After(10 * time.Millisecond):
 			// delete this element
 			s.followers = append(s.followers[:i], s.followers[i+1:]...)
+			log.Printf("De-registered %#v\n", f)
 		}
 	}
 }
 
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/register" {
-		s.registerFollower(w, r)
+		s.serveRegisterFollower(w, r)
 		return
 	}
 }
 
-func (s *Server) registerFollower(w http.ResponseWriter, r *http.Request) {
+func (s *Server) serveRegisterFollower(w http.ResponseWriter, r *http.Request) {
 	body, _ := ioutil.ReadAll(r.Body)
 	var beat Heartbeat
 	json.Unmarshal(body, &beat)
 
+	re := regexp.MustCompile("[[:digit:]]+")
+	lastID := -1
+	// Check if follower already exists.
 	s.mu.RLock()
 	for _, f := range s.followers {
 		if f.ID == beat.ID {
+			// capture ids here to determine next
+			lastID, _ = strconv.Atoi(re.FindString(f.ID))
+
 			// follower is already present
 			return
 		}
 	}
 	s.mu.RUnlock()
 
+	// We have a new follower. Send ssh and host information
+	lastID++
+	hostname := strings.Replace(*hostnamePattern, "ID", strconv.Itoa(lastID), -1)
+
+	// Add the follower to list
 	s.mu.Lock()
 	defer s.mu.Unlock()
-
 	s.followers = append(s.followers, beat)
+	log.Printf("Registered %#v\n", beat)
 }
 
 type Heartbeat struct {
